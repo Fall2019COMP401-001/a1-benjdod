694a4bfa83d76c23de30a93a5d8b504eceb1adfb
benjd <benjd@DESKTOP-BRKTMOG.wireless-1x.unc.edu>
2019-08-26 18:23:36 -0400

    First submission

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -9,6 +9,97 @@ public class A1Jedi {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
-		
+
+		// inventory
+				int inventoryct = scan.nextInt();
+				String[] items = new String[inventoryct];
+				double[] itemprice = new double[inventoryct];
+				int[] itempurch = new int[inventoryct];
+				int[] itemquants = new int[inventoryct];
+				
+				// building the inventory
+				for (int i = 0; i < inventoryct; i++) {
+					items[i] = scan.next().toLowerCase();
+					itemprice[i] = scan.nextDouble();
+				}
+				
+				// customer data initialization
+				int custcount = scan.nextInt();
+				String[] firstname = new String[custcount];
+				String[] lastname = new String[custcount];
+				int itemspurchased = 0;
+				double[] totals = new double[custcount];
+				
+				// customer data input
+				for (int i = 0; i < custcount; i++) {
+					firstname[i] = firstcap(scan.next());
+					lastname[i] = firstcap(scan.next());
+					itemspurchased = scan.nextInt();
+					
+					String item;
+					int itemquant;
+					
+					// gettin their items
+					for (int j = 0; j < itemspurchased; j++) {
+						itemquant = scan.nextInt();
+						item = scan.next();
+						
+						for (int k = 0; k < items.length; k++) {
+							if (item.toLowerCase().equals(items[k])) {
+								itempurch[k] += 1;
+								itemquants[k] += itemquant;
+							}
+						}
+						
+					}
+				}
+				
+				// close the scanner
+				scan.close();
+				
+				for (int i = 0; i < itempurch.length; i++) {
+					if (itempurch[i] == 0) {
+						System.out.println("No customers bought " + firstcap(items[i]));
+					} else {
+						System.out.println(itempurch[i] + " customers bought " + itemquants[i] + " " + firstcap(items[i]));
+					}
+				}
+				
+				/*
+				// output variable initialization
+				double mintotal = totals[0];
+				double maxtotal = totals[0];
+				double averagetotal = 0;
+				int mindex = 0;
+				int maxdex = 0;
+				
+				// min, max total values for the output
+				for (int i = 0; i < totals.length; i++) {
+					if (totals[i] < mintotal) {
+						mintotal = totals[i];
+						mindex = i;
+					}
+					if (totals[i] > maxtotal) {
+						maxtotal = totals[i];
+						maxdex = i;
+					}
+				}
+				
+				// average total value for the output
+				for (int i = 0; i < totals.length; i++) {
+					averagetotal += totals[i];
+				}
+				averagetotal = averagetotal/totals.length;
+				
+				// output!!	
+				System.out.println("Biggest: " + firstname[maxdex] + " " + lastname[maxdex] + " (" + String.format("%.2f", maxtotal) + ")");
+				System.out.println("Smallest: " + firstname[mindex] + " " + lastname[mindex] + " (" + String.format("%.2f", mintotal) + ")");
+				System.out.println("Average: " + String.format("%.2f", averagetotal));
+				*/
 	}
+	
+	// firstcap method (for names)
+		public static String firstcap(String s) {
+			return s.toUpperCase().charAt(0) + s.substring(1).toLowerCase();
+		}
 }
--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,5 +10,41 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		// customer count
+		int customer_count = scan.nextInt();
+		
+		// initialize aligned lists for all necessary values 
+		String[] firstname = new String[customer_count];
+		String[] lastname = new String[customer_count];
+		int[] itemct = new int[customer_count];
+		double[] itemtotal = new double[customer_count];
+		
+		// customer data filling loop 
+		for (int i = 0; i < customer_count; i++) {
+			firstname[i] = scan.next();
+			lastname[i] = scan.next();
+			itemct[i] = scan.nextInt();
+			itemtotal[i] = 0;
+			
+			// item loop for each customer
+			// don't need the names or quantities of the items!! 
+			for (int j = 0; j < itemct[i]; j++) {
+				scan.nextInt(); // throwaway quantity
+				scan.next(); // throwaway item name
+				itemtotal[i] += scan.nextDouble(); 
+			}
+		}
+		
+		// closing the scanner
+		scan.close();
+		
+		// output
+		for (int k = 0; k < customer_count; k++) {
+			System.out.print(firstname[k].toUpperCase().charAt(0) + ". ");
+			System.out.print(lastname[k].toUpperCase().charAt(0) + lastname[k].toLowerCase().substring(1) + ": ");
+			System.out.print(String.format("%.2f", itemtotal[k]));
+			System.out.print("\n");
+		}
+		
 	}
 }
